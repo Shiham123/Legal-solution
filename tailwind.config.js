7cf97196efab69d70611ab38cd1ed3b8bc834773
Shiham123 <s36651786@gmail.com>
2023-07-24 20:07:53 +0600

    navbar and banner section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': { max: '1535px' },
+        xl: { max: '1279px' },
+        lg: { max: '1023px' },
+        md: { max: '767px' },
+        sm: { max: '439px' },
+      },
+    },
   },
   plugins: [],
 };
